571ae1d085f581cc099319f92434c1091fc50359
Luke Owlclaw <2737900+LukeOwlclaw@users.noreply.github.com>
2018-08-18 21:40:05 +0200

    - added missing VisibleWhenTrueConverter.cs

---
+++ Shared/Converters/VisibleWhenTrueConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Shared.Converters
+{
+    public class VisibleWhenTrueConverter : IValueConverter
+    {
+        public object Convert(object v, Type t, object p, string l)
+        {
+            return Equals(true, System.Convert.ToBoolean(v)) ? Visibility.Visible : Visibility.Collapsed;
+        }
+        public object ConvertBack(object v, Type t, object p, string l) => null;
+    }
+}
