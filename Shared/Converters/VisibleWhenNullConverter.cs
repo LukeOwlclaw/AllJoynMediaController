86b305168819e083a782d52430ee39b02e6ad11e
Luke Owlclaw <2737900+LukeOwlclaw@users.noreply.github.com>
2018-09-06 10:11:26 +0200

    - show more details about devices - show warning when Interfaces is null

---
+++ Shared/Converters/VisibleWhenNullConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Shared.Converters
+{
+    public class VisibleWhenNullConverter : IValueConverter
+    {
+        public object Convert(object v, Type t, object p, string l) =>
+        v == null ? Visibility.Visible : Visibility.Collapsed;
+
+        public object ConvertBack(object v, Type t, object p, string l) => null;
+    }
+}
